8b08481cd1d8c7d0d54fdbe7b847d4f656066028
EIDKARI <94180181+EIDKARI@users.noreply.github.com>
2022-01-11 10:56:08 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,13 +5,5 @@ mkdir genfile
 cd %CD%\genfile
 rem create a new jenkins artifact
 copy nul > jenkins_file.txt
-echo Test 1 = SUCCESS > jenkins_file.txt
-echo:
-echo Test 2 = SUCCESS > jenkins_file.txt
-echo:
-echo Test 3 = SUCCESS > jenkins_file.txt
-echo:
-echo Test 4 = SUCCESS > jenkins_file.txt
-echo:
-echo Test 5 = FAIL  > jenkins_file.txt
-echo:
+echo test > jenkins_file.txt
+
