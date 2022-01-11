6bebdf13af355f01c0fb1ba6f9f2cb46ada17bf2
EIDKARI <94180181+EIDKARI@users.noreply.github.com>
2022-01-11 11:11:36 +0100

    added files newly

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@ echo "jenkins node is connected"
 cd %CD%
 if exist %CD%\genfile\ rmdir /s /q %CD%\genfile\
 mkdir genfile
+copy output.xml %CD%\genfile
 cd %CD%\genfile
 rem create a new jenkins artifact
 copy nul > jenkins_file.txt
 echo test > jenkins_file.txt
-
