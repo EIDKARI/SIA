ea28b21c93d0b486eb7aa2d08e7f55e2494970b5
EIDKARI <94180181+EIDKARI@users.noreply.github.com>
2022-01-11 09:33:16 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,2 @@
-echo "jenkins node is connected"
-cd %CD%
-exist %CD%\genfile\ rmdir /s /q %CD%\genfile\
-mkdir genfile
-cd %CD%\genfile
-rem create a new jenkins artifact
 copy nul > jenkins_file.txt
 echo This file has been created by jenkins! > jenkins_file.txt
